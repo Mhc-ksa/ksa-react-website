4c8ba6c1647020137c60cb31a6860390c15c6d25
hejin-jeong <pearlofthesea25@gmail.com>
2022-01-31 22:49:16 -0500

    host react website using firebase

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -4,9 +4,9 @@ import './JoinNow.css'
 function JoinNow() {
     return (
         // TODO: change to sign up link
-        <div className="flex container-join-now align-center">
+        <div className="flex container-join-now align-center ">
             <div>
-                <p className="font-m gray">Enter your email adress</p>
+                <input className="font-m gray join-now-input" placeholder="Enter your email adress" />
             </div>
             <div className="btn-join-now font-m">
                 <a href="https://www.instagram.com/mhcksa/" className="atag-link-to-join"><p>Join Now</p></a>
